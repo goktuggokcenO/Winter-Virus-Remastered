17e96cb37a23b8858965f0884dbce30f5935731e
Göktuğ Gökcen <102878455+goktuggokcenO@users.noreply.github.com>
2025-02-04 13:51:38 +0300

    Add player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    public float playerSpeed;
+    public float jumpingPower;
+    private bool isFacingRight = true;
+
+    // Data inputs.
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    private float horizontal;
+    private float movementDetect;
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.freezeRotation = true;
+    }
+
+    private void Update()
+    {
+        GetInput();
+        Flip();
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void GetInput()
+    {
+        // Get player input.
+        horizontal = Input.GetAxisRaw("Horizontal");
+
+        // Jup if player input is jump key.
+        if (Input.GetButtonDown("Jump") && isGrounded())
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpingPower);
+        }
+        if (Input.GetButtonUp("Jump") && rb.linearVelocity.y > 0f)
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, rb.linearVelocity.y * 0.5f);
+        }
+    }
+
+    private void MovePlayer()
+    {
+        rb.linearVelocity = new Vector2(horizontal * playerSpeed, rb.linearVelocity.y);
+    }
+
+    private void Flip()
+    {
+        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
+    }
+
+    private bool isGrounded()
+    {
+        return Physics2D.Raycast(groundCheck.position, Vector2.down, 0.2f, groundLayer);
+    }
+}
