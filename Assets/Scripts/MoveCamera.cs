5c5604442339d1bd0f1437738e365570c3ece224
Göktuğ Gökcen <102878455+goktuggokcenO@users.noreply.github.com>
2025-02-13 00:34:48 +0300

    Add camera movement

---
+++ Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,12 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    public Transform cameraPosition;
+
+    private void Update()
+    {
+        transform.position = cameraPosition.position;
+    }
+}
